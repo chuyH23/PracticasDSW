376c8687e1972c22e994340fa7312670ce087b5d
chuyH23 <134812445+chuyH23@users.noreply.github.com>
2023-10-03 18:01:16 -0700

    primera practica con js donde probamos operaciones matematicas

---
+++ practica24/javaIinicio.js
@@ -0,0 +1,54 @@
+/*
+var num1= prompt("ingrese el primer numero");
+var num2=prompt("ingrese el segundo numero");
+
+
+
+var numero1 = parseint(num1);
+var numero2 = parseint(num2);
+var resultado = (num1+num2);
+
+alert(resultado);
+
+*/
+/*
+var mensaje = "bienvenido a nuestro sitio web";
+var texto1 = " texto comillas dobles,";
+var texto3 = " texto comillas simples....";
+
+alert(mensaje+ texto1 +texto3 );*/
+
+/*
+var clienteRegistrado =false;
+var ivaRegistrado =true;
+
+alert(clienteRegistrado);
+*/
+
+/*
+var array = [`tijuana`,`rosarito`,`ensenada`,`mexicali`];
+alert(array);
+*/
+
+
+//alert(array[3]);
+
+
+var num1= prompt("ingrese el primer numero");
+var num2=prompt("ingrese el segundo numero");
+
+var numero1 = parseFloat(num1);
+var numero2 = parseFloat(num2);
+
+
+var suma = (numero1+ numero2);
+var resta = (numero1-numero2);
+var multiplicacion = (numero1* numero2);
+var division= (numero1/ numero2);
+
+var mensaje = "Suma = " + suma + "\nResta = " + resta + "\nMultiplicación = " + multiplicacion + "\nDivisión = " + division;
+
+alert(mensaje);
+
+//JESUS HERNANDEZ RUIZ
+
